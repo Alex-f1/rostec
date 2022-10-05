e5a4b86ccde670d8b24a7890d5248eaf38a28b5e
Alex <skyriver.f1@gmail.com>
2022-10-05 12:49:52 +0500

    added full menu

---
+++ marmelad/_blocks/top-panel/top-panel.js
@@ -0,0 +1,16 @@
+$('.js-button-burger').on('click', function () {
+  $(this).toggleClass('_is-active');
+  $('.full-menu').toggleClass('_is-show');
+  // $('html').toggleClass('_is-lock');
+});
+
+$('.js-button-search').on('click', function (event) {
+  event.preventDefault();
+  $('.top-line .search-panel').addClass('_is-show');
+});
+
+$('.js-button-close').on('click', function (event) {
+  $('.top-line .search-panel').removeClass('_is-show');
+});
+
+$('.full-menu').show();
